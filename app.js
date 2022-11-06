5e93819a0a271fddf4adc73e4ea09bfe532c62af
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-11-06 20:54:34 +0200

    commit before blackout

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ const bodyParser = require("body-parser"); // npm i body-parser
 
 const app = express(); // create app constant by using express
 
+app.use('view engine', 'ejs'); //this line of code tells our app which is generated using Express to use EJS
+
 app.get("/", function(req, res){ // Creatin GET route
-	res.send("Hello"); // sends browsing the word "Hello", when a user tries to accsess the home route
+
+//	res.send("Hello"); // sends browsing the word "Hello", when a user tries to accsess the home route
+
+
+const today = new Date();
+const currentDay = today.getDay();
+const day = "";
+
+if (currentDay === 6 || currentDay === 0) {
+  // res.write("<h1>Yay it's the weekend! YUIIIIII!!!</h1>");
+  // res.write("<p>I haven't to work!</p>");
+  day = "Weekend";
+    res.sendFile(__dirname + "/index.html");
+} else {
+  day = "Weekday";
+  res.sendFile(__dirname + "/index.html");
+}
+
+
 });
 
 app.listen(3000, function(){ // isten on port 3000
